86e2d4cd7ee8415f5c46995b1a1dc432d72f86de
dave-doyle <123418208+dave-doyle@users.noreply.github.com>
2023-08-27 21:14:35 +0100

    added typewriter animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,50 @@
 import React from "react";
 import Scrambler from "./Scrambler.js";
+import { TypeAnimation } from "react-type-animation";
 
 function About() {
-  const phrases = ["THIS IS A TEST PHRASE TO SCRAMBLE"]; // Array of phrases
-  const initialDelay = 1; // Adjust this value
-  const revealDuration = 10000; // Adjust this value
+  const phrase_1 = ["David Doyle Software Developer"]; // Array of phrases
+
+  
+  const initialDelay = 5; 
+  const revealDuration = 2000; 
 
   return (
     <section id="about">
       <h2>About Me</h2>
-      <p>
-        This is the About section of your portfolio. You can provide information
-        about yourself here.
-      </p>
-      <div className="container">
+
+      {/* <div className="container">
         <div className="text">
           <Scrambler
-            phrases={phrases}
+            phrases={phrase_1}
             initialDelay={initialDelay}
             revealDuration={revealDuration}
           />
         </div>
-      </div>
+      </div> */}
+
+      <br />
+
+      
+        
+        <div className="revealed-text">
+          <div className="container">
+            
+              <Scrambler
+                phrases={phrase_1}
+                initialDelay={initialDelay}
+                revealDuration={revealDuration}
+              />
+            
+            <TypeAnimation
+              sequence={[``, 1, ""]}
+              speed={20}
+              style={{ whiteSpace: "pre-line", fontSize: "2em" }}
+              repeat={0}
+            />
+          </div>
+        </div>
+      
     </section>
   );
 }
--- src/components/Scrambler.js
+++ src/components/Scrambler.js
@@ -6,6 +6,7 @@ const Scrambler = ({ phrases, initialDelay, revealDuration }) => {
     "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン漢字文学科学生物化学歴史地理音楽美術数学社会政治経済外国語";
   const [displayedText, setDisplayedText] = useState("");
   const [frame, setFrame] = useState(0);
+  const [scrambleCompleted, setScrambleCompleted] = useState(false);
 
   useEffect(() => {
     const update = () => {
@@ -22,27 +23,24 @@ const Scrambler = ({ phrases, initialDelay, revealDuration }) => {
 
       setDisplayedText(newDisplayedText);
 
-      if (frame < initialDelay + fx.length * revealDuration) {
+      if (!scrambleCompleted && frame >= initialDelay + fx.length * revealDuration) {
+        setScrambleCompleted(true);
+      }
+
+      if (!scrambleCompleted) {
         setFrame((prevFrame) => prevFrame + 1);
         requestAnimationFrame(update);
       } else {
         setTimeout(() => {
-          let newChar = chars[Math.floor(Math.random() * chars.length)];
-          setDisplayedText((prevText) => {
-            const newText =
-              prevText.slice(0, prevText.length - 1) + newChar;
-            return newText;
-          });
-          if (newChar === fx[fx.length - 1]) {
-            setCounter((prevCounter) => (prevCounter + 1) % phrases.length);
-            setFrame(0); // Reset frame for the next phrase
-          }
+          setCounter((prevCounter) => (prevCounter + 1) % phrases.length);
+          setDisplayedText(""); // Reset the displayed text
+          setScrambleCompleted(false); // Reset the scrambleCompleted state
         }, 800);
       }
     };
 
     update();
-  }, [counter, phrases, frame, chars, initialDelay, revealDuration]);
+  }, [counter, phrases, frame, chars, initialDelay, revealDuration, scrambleCompleted]);
 
   return <div className="text">{displayedText}</div>;
 };
