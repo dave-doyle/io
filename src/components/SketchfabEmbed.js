790bc9770097cfbaae42fa3e075fef8d437b9444
dave-doyle <123418208+dave-doyle@users.noreply.github.com>
2023-08-30 03:36:58 +0100

    3d model added adn fixed

--- src/components/SketchfabEmbed.js
+++ src/components/SketchfabEmbed.js
@@ -1,45 +1,53 @@
-import React, { useEffect } from 'react';
-import Sketchfab from '@sketchfab/viewer-api';
+import React, { useEffect } from "react";
+import Sketchfab from "@sketchfab/viewer-api";
 
 const SketchfabEmbed = () => {
   useEffect(() => {
-    const iframe = document.getElementById('sketchfab-iframe');
-    const uid = '97cb73804ec64963863317559eb832e0';
+    const iframe = document.getElementById("sketchfab-iframe");
+    const uid = "97cb73804ec64963863317559eb832e0";
 
-    const client = new Sketchfab('1.12.1', iframe);
+    const client = new Sketchfab("1.12.1", iframe);
 
     client.init(uid, {
       success: function onSuccess(api) {
         api.start();
-        api.addEventListener('viewerready', function () {
-          console.log('Viewer is ready');
-          
-         // Define the camera position and target
-const cameraPosition = [50, -50, 60]; // X, Y, Z coordinates
-const cameraTarget = [0, 0, 10];    // X, Y, Z coordinates
-const duration = 0.5; // Duration of the camera move in seconds
+        api.addEventListener("viewerready", function () {
+          console.log("Viewer is ready");
 
+          // Define the camera position and target
+          const cameraPosition = [50, -50, 60]; // X, Y, Z coordinates
+          const cameraTarget = [0, 0, 10]; // X, Y, Z coordinates
+          const duration = 0.5; // Duration of the camera move in seconds
 
           // Set the camera view
-          api.setCameraLookAt(cameraPosition, cameraTarget, duration, function(err) {
-            if (!err) {
-              console.log('Camera view set');
+          api.setCameraLookAt(
+            cameraPosition,
+            cameraTarget,
+            duration,
+            function (err) {
+              if (!err) {
+                console.log("Camera view set");
+              }
             }
-          });
+          );
 
           api.load(); // Load the model
         });
       },
       error: function onError() {
-        console.log('Viewer error');
+        console.log("Viewer error");
       },
       autostart: 1, // Start the model immediately
-      camera: 5,    // Use camera preset 5
+      camera: 5, // Use camera preset 5
     });
   }, []);
 
   return (
-    <div id="sketchfab-container" className="sketchfab-embed-wrapper">
+    <div
+      id="sketchfab-container"
+      className="sketchfab-embed-wrapper"
+      style={{ position: "fixed", width: "18%", height: "100%" }}
+    >
       <iframe
         id="sketchfab-iframe"
         title="Sketchfab Viewer"
@@ -55,7 +63,20 @@ const duration = 0.5; // Duration of the camera move in seconds
         width="100%"
         height="100%"
         src="" // Initially empty
+        position="fixed"
+        controls="0" // Disable default controls
       />
+      <div
+          style={{
+            position: "absolute",
+            bottom: "0",
+            left: "0",
+            width: "100%",
+            height: "220px", // Adjust the height as needed to cover the watermark
+            background: "black", // Change the color as needed
+          }}
+        />
+      
     </div>
   );
 };
