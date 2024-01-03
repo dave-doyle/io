4000e21ce5e7c9222c2a30e1fda11c06225d7790
dave-doyle <123418208+dave-doyle@users.noreply.github.com>
2024-01-03 21:27:44 +0000

    mobile menu outside click bug fixed

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,5 +1,5 @@
 // MobileMenu component for the hamburger menu on mobile devices
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Menu, MenuButton, MenuList, IconButton } from "@chakra-ui/react";
 import { HamburgerIcon} from '@chakra-ui/icons'
 import { motion } from "framer-motion";
@@ -9,6 +9,7 @@ import "../MobileMenu.css";
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const menuRef = useRef();
 
   useEffect(() => {
     const visibilityTimeout = setTimeout(() => {
@@ -18,10 +19,27 @@ const MobileMenu = () => {
     return () => clearTimeout(visibilityTimeout);
   }, []);
 
+  // when you click outside the mobile menu + close it and then try to reopen, this allows that
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   // Handle toggling menu visibility
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+ 
+  
 
   return (
     <Menu>
@@ -32,7 +50,7 @@ const MobileMenu = () => {
         variant="outline"
         onClick={toggleMenu}
       />
-      <MenuList display={isOpen ? "block" : "none"}>
+      <MenuList display={isOpen ? "block" : "none"} ref={menuRef}>
         {/* Mobile-specific menu items */}
         {isMenuVisible && (
   <motion.div
@@ -43,22 +61,22 @@ const MobileMenu = () => {
   >
     <ul>
       <li>
-        <Link to="/" className="menu-link">
+        <Link to="/" className="menu-link" >
           Home
         </Link>
       </li>
       <li>
-        <Link to="/about_me" className="menu-link">
+        <Link to="/about_me" className="menu-link" >
           About Me
         </Link>
       </li>
       <li>
-        <Link to="/projects" className="menu-link">
+        <Link to="/projects" className="menu-link" >
           Projects
         </Link>
       </li>
       <li>
-        <Link to="/contact" className="menu-link">
+        <Link to="/contact" className="menu-link" >
           Contact
         </Link>
       </li>
