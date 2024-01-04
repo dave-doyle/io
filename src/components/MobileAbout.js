ea69255ed985692b49eabab53d51c92eddbcda95
dave-doyle <123418208+dave-doyle@users.noreply.github.com>
2024-01-04 01:00:42 +0000

    changing mobile about animation

--- src/components/MobileAbout.js
+++ src/components/MobileAbout.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 import Scrambler from "./Scrambler.js";
 import { motion} from "framer-motion";
-import RevealText from "./RevealText.js";
 
 function MobileAbout() {
   const phrase_1 = ["David Doyle"];
   const phrase_2 = ["Software Developer"]; // Array of phrases
   const phrase_3 = ["From Dublin Ireland"];
 
-  const initialDelay = 20;
+  const initialDelay = 500;
   const revealDuration = 80;
+
+  const initialDelay2 = 1200;
+  const revealDuration2 = 80;
 
   const [isGrayLine1Visible, setIsGrayLine1Visible] = useState(false);
   const [isGrayLine2Visible, setIsGrayLine2Visible] = useState(false);
@@ -27,12 +29,11 @@ function MobileAbout() {
   }, []);
 
   return (
+    <SimpleGrid columns={[1, null, 1]} spacing={4} margin="auto" maxWidth="90%">
     <Box
       id="about"
       color="white"
-    //   marginBottom={"1vh"}
       marginTop={"10vh"}
-    //   marginLeft={"10vw"}
     >
       <Heading
         fontWeight={"extrabold"}
@@ -43,7 +44,48 @@ function MobileAbout() {
         paddingBottom={"2vh"}
         color={"#a2a1a1"}
       >
-        <Scrambler
+        
+
+        
+
+       
+
+        {/* black background top*/}
+        <motion.div
+          initial={{ width: 0 }}
+          animate={{ width: "65vw" }}
+          transition={{ duration: 1.2, delay: 3 }} // Adjust the duration as needed
+          style={{
+            height: "30vh",
+            
+            background: "black",
+            position: "absolute",
+            top: "25vw",
+            left: "35vw",
+            zIndex: -1,
+            borderRadius: "3px",
+          }}
+        >
+
+        
+        
+        </motion.div>
+        {/* grey line top*/}
+        <motion.div
+          initial={{ width: 0 }}
+          animate={{ width: "45vw" }}
+          transition={{ duration: 2.0, delay: 3 }} // Adjust the duration as needed
+          style={{
+            height: "1px",
+            background: "black",
+            position: "absolute",
+            top: "35vw",
+            left: "55vw",
+            
+            
+          }}
+        >
+            <Scrambler
           phrases={phrase_1}
           initialDelay={initialDelay}
           revealDuration={revealDuration}
@@ -53,57 +95,28 @@ function MobileAbout() {
           
           
         />
-        <motion.div
-          initial={{ width: 0 }}
-          animate={{ width: "51vw" }}
-          transition={{ duration: 2.0 }} // Adjust the duration as needed
-          style={{
-            height: "1px",
-            background: "grey",
-            position: "absolute",
-            top: "39vw",
-            left: "8vw",
-            
-          }}
-        ></motion.div>
+        </motion.div>
+      </Heading>
 
-        <motion.div
+      
+
+      <motion.div
           initial={{ width: 0 }}
-          animate={{ width: "93vw" }}
+          animate={{ width: "90vw" }}
           transition={{ duration: 1.0 }} // Adjust the duration as needed
           style={{
             height: "20vh",
             background: "black",
             position: "absolute",
             top: "50vw",
-            left: "1vw",
+            left: "10vw",
             zIndex: -1,
             borderRadius: "3px",
           }}
-        ></motion.div>
+        >
 
-        {/* black background top*/}
-        <motion.div
-          initial={{ width: 0 }}
-          animate={{ width: "90vw" }}
-          transition={{ duration: 2.2 }} // Adjust the duration as needed
-          style={{
-            height: "10vh",
-            
-            background: "black",
-            position: "absolute",
-            top: "25vw",
-            left: "5vw",
-            zIndex: -1,
-            borderRadius: "3px",
-          }}
-        ></motion.div>
-      </Heading>
-
-      <br></br>
-
-        <div className="container" style={{ display: "flex" }}>
-          <Text
+        {/* <div className="container" style={{ display: "flex" }}> */}
+        {/* <Text
             borderRadius={"3px"}
             paddingRight={"4vw"}
             paddingLeft={"50v"}
@@ -112,19 +125,19 @@ function MobileAbout() {
             fontWeight={900}
             fontFamily="Font2, roboto mono"
             marginLeft={-9}
-          >
+          > */}
             <Scrambler
               phrases={phrase_2}
-              initialDelay={initialDelay}
-              revealDuration={revealDuration}
-              fontSize="8em"
+              initialDelay={initialDelay2}
+              revealDuration={revealDuration2}
+              fontSize="2xl"
               fontFamily="roboto mono"
             />
-          </Text>
+          {/* </Text> */}
           
-        </div>
-        <div className="container" style={{ display: "flex" }}>
-          <Text
+        {/* </div> */}
+        {/* <div className="container" style={{ display: "flex" }}> */}
+          {/* <Text
             borderRadius={"3px"}
             paddingRight={"4vw"}
             paddingLeft={"50v"}
@@ -133,21 +146,23 @@ function MobileAbout() {
             fontWeight={900}
             fontFamily="Font2, roboto mono"
             marginLeft={-9}
-          >
+          > */}
             <Scrambler
               phrases={phrase_3}
-              initialDelay={initialDelay}
-              revealDuration={revealDuration}
-              fontSize="8em"
+              initialDelay={initialDelay2}
+              revealDuration={revealDuration2}
+              fontSize="10em"
               fontFamily="roboto mono"
             />
-          </Text>
+          {/* </Text> */}
+          </motion.div>
           
-        </div>
-      <br></br>
+        {/* </div> */}
+      
 
       
     </Box>
+    </SimpleGrid>
   );
 }
 
