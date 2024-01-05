700aac83cf3338e15d84631a85509ab5456812ec
dave-doyle <123418208+dave-doyle@users.noreply.github.com>
2024-01-05 14:00:11 +0000

    Mobile menu fully fixed

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,96 +1,61 @@
-// MobileMenu component for the hamburger menu on mobile devices
-import React, { useState, useEffect, useRef } from "react";
-import { Menu, MenuButton, MenuList, IconButton } from "@chakra-ui/react";
-import { HamburgerIcon} from '@chakra-ui/icons'
-import { motion } from "framer-motion";
-import { Link, useLocation } from "react-router-dom";import "../MobileMenu.css";
+import React, { useState } from "react";
+import { Menu, MenuButton, MenuList, MenuItem, IconButton } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
 
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef();
-
-  // useEffect(() => {
-  //   const handleClickOutside = (event) => {
-  //     if (menuRef.current && !menuRef.current.contains(event.target)) {
-  //       setIsOpen(false);
-  //     }
-  //   };
-
-  //   document.addEventListener("mousedown", handleClickOutside);
-  //   return () => {
-  //     document.removeEventListener("mousedown", handleClickOutside);
-  //   };
-  // }, []);
-
-  useEffect(() => {
-    console.log(isOpen, 'current isOpen state');
-  }, [isOpen]);
-
-  const toggleMenu = () => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
-  };
 
   const closeMenu = () => {
     setIsOpen(false);
   };
 
   return (
-    <div style={{ zIndex: 10 }}>
-      <button
+    <Menu isOpen={isOpen} onClose={closeMenu}>
+      <MenuButton
+        as={IconButton}
         aria-label="Options"
+        icon={<HamburgerIcon />}
         variant="outline"
-        onClick={toggleMenu}
+        onClick={() => setIsOpen(!isOpen)}
         style={{
-          width: '15vw',
-          height: '5vh',
-          top: '0vh',
-          right: '0vw',
-          color: 'white',
+          width: "15vw",
+          height: "5vh",
+          top: "0vh",
+          right: "0vw",
+          color: "white",
           zIndex: 10,
-          backgroundColor: 'transparent',
-          border: 'none',
-          cursor: 'pointer',
         }}
-      >
-        <HamburgerIcon />
-      </button>
-      <div
-        className="menu"
+      />
+      <MenuList
         style={{
-          display: isOpen ? "block" : "none",
-          zIndex: 10,
-          backgroundColor: 'red',
-          position: 'absolute',
-          top: '5vh', // Adjust positioning as needed
-          right: '0',
+          backgroundColor: "black",
+          top: "6vh", // Adjust positioning as needed
         }}
-        ref={menuRef}
       >
         {/* Mobile-specific menu items */}
-        <ul>
-          <li>
-            <Link to="/" className="menu-link" onClick={closeMenu}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/about_me" className="menu-link" onClick={closeMenu}>
-              About Me
-            </Link>
-          </li>
-          <li>
-            <Link to="/projects" className="menu-link" onClick={closeMenu}>
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="menu-link" onClick={closeMenu}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+        <MenuItem onClick={closeMenu}>
+          <Link to="/" className="menu-link">
+            Home
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={closeMenu}>
+          <Link to="/about_me" className="menu-link">
+            About Me
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={closeMenu}>
+          <Link to="/projects" className="menu-link">
+            Projects
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={closeMenu}>
+          <Link to="/contact" className="menu-link">
+            Contact
+          </Link>
+        </MenuItem>
+      </MenuList>
+    </Menu>
   );
 };
 
